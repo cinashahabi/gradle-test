8497ebf65004adc8c1fd7bc81501697f30f8fe46
Cina Shahabi <Cina.Shahabi@veritas.com>
2017-06-14 14:45:55 -0700

    Working build.gradle file

--- src/main/java/javaApp/WordSplit.java
+++ src/main/java/javaApp/WordSplit.java
@@ -17,18 +17,21 @@
 * from Adobe Systems Incorporated.
 */
 
-import java.util.Arrays;
-import org.apache.commons.lang.StringUtils;
+package javaApp;
 
-public class javaApp {
+import java.util.Arrays;
+import org.apache.commons.lang3.StringUtils;
+
+public class WordSplit {
 
 	public static String[] splitWord(String word) {
+		word = StringUtils.swapCase(word);
 		String[] split = word.split("");
 		return split;
 	}
 
 	public static void main(String[] args) {
-	 	if (args == null) {
+	 	if (args.length == 0) {
 	 		System.out.println("No words to split, hombre.");
 	 	}
 		for (int i = 0; i < args.length; i+=1) {
